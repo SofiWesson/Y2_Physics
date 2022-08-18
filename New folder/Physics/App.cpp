966111955e804d202c2ceda9d9b584c03c975969
Sofi <w0lfskitten173@gmail.com>
2022-08-18 12:33:04 +1000

    updated bin file path for source to work

--- New folder/Physics/App.cpp
+++ New folder/Physics/App.cpp
@@ -29,8 +29,8 @@ bool App::startup()
 	m_2dRenderer = new aie::Renderer2D();
 
 	// TODO: remember to change this when redistributing a build!
-	// the following path would be used instead: "./font/consolas.ttf"
-	m_font = new aie::Font("./bin/font/consolas.ttf", 32);
+	// the following path would be used instead: "./bin/font/consolas.ttf"
+	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
 
 	// ===================================GAME STATE MANAGER==============================
 	m_GSM = new GameStateManager();
